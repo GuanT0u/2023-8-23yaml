e09eef3b436ae857ec363775a19203b30e0f9b41
GuanT0u <137639949+GuanT0u@users.noreply.github.com>
2023-08-24 09:44:33 +0800

    Add files via upload

---
+++ common/tools.py
@@ -0,0 +1,19 @@
+import os
+
+def get_project_path():
+    project_name = "2023"
+    filepath = os.path.dirname(__file__)
+    return filepath[:filepath.find(project_name) + len(project_name)]
+
+def sep(path, add_sep_before=False, add_sep_after=False):
+    all_path = os.sep.join(path)
+    if add_sep_before:
+        all_path = os.sep + all_path
+    if add_sep_after:
+        all_path = all_path + os.sep
+    return all_path
+
+
+if __name__ == "__main__":
+    print(sep(["config", "environment.yaml"], True))
+
---
+++ common/yaml_config.py
@@ -0,0 +1,21 @@
+import yaml
+from common.tools import get_project_path, sep
+
+
+class GetConfig():
+    def __init__(self):
+        # with open("../config/environment.yaml", "r", encoding="utf-8") as env_file:
+        with open(get_project_path() + sep(["config", "environment.yaml"], True), "r", encoding='utf-8') as env_file:
+            self.env = yaml.load(env_file, Loader=yaml.FullLoader)
+            print(self.env)
+
+    def get_username_password(self):
+        return self.env["username"], self.env["password"]
+
+
+if __name__ == "__main__":
+    print(GetConfig().get_username_password())
+
+
+
+
